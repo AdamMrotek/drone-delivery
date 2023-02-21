4394932d4589be37ad45ff5328045840ecfaf0a1
Adam Mrotek <adam.mrotek@gmail.com>
2023-02-21 23:04:14 +0000

    Add PWA

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+});
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  pwa: {
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+};
+// const pwaConfig = withPWA(nextConfig);
+// delete nextConfig.pwa;
+// module.exports = nextConfig;
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
