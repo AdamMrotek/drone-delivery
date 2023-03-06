2c44012ea0f5ed5afa73fe6b949ab897a85be277
Adam Mrotek <adam.mrotek@gmail.com>
2023-03-06 23:48:28 +0000

    Stop and restart functions

--- components/base/DroneController.tsx
+++ components/base/DroneController.tsx
@@ -1,11 +1,13 @@
 import { doc, setDoc } from "firebase/firestore";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import { db } from "../../configs/firebase/firebaseConfig";
 interface Position {
   lat: number;
   lng: number;
 }
+
 export function DroneController() {
+  const intervalRef = useRef();
   const guysHospitalCoordinates: Position = {
     lat: 51.5024498412395,
     lng: -0.08787809742533845,
@@ -14,37 +16,34 @@ export function DroneController() {
     lat: 51.49920431248376,
     lng: -0.11887389031303706,
   };
-
-  const droneAutoController = useCallback(
-    (pathFractions: number, signalFrequency: number) => {
-      function getNewPosition(
-        pathFraction: number,
-        direction: "forward" | "back"
-      ): Position {
-        const THC = thomasHospitalCoordinates;
-        const GHC = guysHospitalCoordinates;
-        const currentPosition =
-          direction === "forward" ? pathFraction : 1 - pathFraction;
-        return {
-          lat: THC.lat - (THC.lat - GHC.lat) * currentPosition,
-          lng: THC.lng - (THC.lng - GHC.lng) * currentPosition,
-        };
-      }
-      let desCounter = 0;
-      const positionInterval = setInterval(() => {
-        const direction =
-          Math.floor(desCounter / pathFractions) % 2 === 0 ? "forward" : "back";
-        const currentFraction = (desCounter % pathFractions) / pathFractions;
-        const newPosition = getNewPosition(currentFraction, direction);
-        dispachDrone(newPosition);
-        desCounter++;
-      }, signalFrequency);
-      return () => clearInterval(positionInterval);
-    },
-    [guysHospitalCoordinates, thomasHospitalCoordinates]
-  );
-
+  function getNewPosition(
+    pathFraction: number,
+    direction: "forward" | "back"
+  ): Position {
+    const THC = thomasHospitalCoordinates;
+    const GHC = guysHospitalCoordinates;
+    const currentPosition =
+      direction === "forward" ? pathFraction : 1 - pathFraction;
+    return {
+      lat: THC.lat - (THC.lat - GHC.lat) * currentPosition,
+      lng: THC.lng - (THC.lng - GHC.lng) * currentPosition,
+    };
+  }
   let desCounter = 0;
+  const droneAutoController = (
+    pathFractions: number,
+    signalFrequency: number
+  ) => {
+    return () => {
+      const direction =
+        Math.floor(desCounter / pathFractions) % 2 === 0 ? "forward" : "back";
+      const currentFraction = (desCounter % pathFractions) / pathFractions;
+      const newPosition = getNewPosition(currentFraction, direction);
+      dispachDrone(newPosition);
+      desCounter++;
+    };
+  };
+
   async function dispachDrone(position: Position) {
     const cityRef = doc(db, "drones", "FirstDrone");
     setDoc(cityRef, { position: position }, { merge: true });
@@ -64,32 +63,47 @@ export function DroneController() {
       console.error("Error adding document: ", e);
     }
   }
+  const restartDrone = () => {
+    const setDrone = droneAutoController(15, 5000);
+    (intervalRef.current as ReturnType<typeof setInterval> | undefined) =
+      setInterval(() => {
+        console.log("NEW INTERVAL");
+        setDrone();
+      }, 5000);
+    const interval = intervalRef.current;
+  };
   useEffect(() => {
-    const cleanInterval = droneAutoController(15, 5000);
+    const setDrone = droneAutoController(15, 5000);
+    console.log("Interval Set Up");
+    (intervalRef.current as ReturnType<typeof setInterval> | undefined) =
+      setInterval(() => {
+        console.log("NEW INTERVAL");
+        setDrone();
+      }, 5000);
+    const interval = intervalRef.current;
 
     return () => {
-      cleanInterval();
+      console.log("CLeaning");
+      clearInterval(interval);
     };
-  }, [droneAutoController]);
+  }, []);
 
   return (
     <div>
       <button
         className="btn py-2 px-4 m-4 border-solid border rounded-2xl hover:bg-slate-800 hover:text-white duration-300"
-        onClick={() => droneAutoController(10, 3000)}
+        onClick={() => {
+          clearInterval(intervalRef.current);
+        }}
       >
-        Dispach Drone
+        Stop drone
       </button>
+
       <button
         className="btn py-2 px-4 m-4 border-solid border rounded-2xl hover:bg-slate-800 hover:text-white duration-300"
-        onClick={() =>
-          createDrone(
-            thomasHospitalCoordinates.lat,
-            thomasHospitalCoordinates.lng
-          )
-        }
+        onClick={() => restartDrone()}
       >
-        Create Drone
+        Restart drone
       </button>
     </div>
   );
